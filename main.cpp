7a4df4ab55fa706ea2569e20aca49e23b66a240b
Eslam Hesham <48475174+EslamHesham404@users.noreply.github.com>
2019-09-18 20:52:31 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,56 +1,106 @@
-
 #include <iostream>
 #include <cstdlib>
+#include <string>
 #include <ctime>
+
+void print_array(int arr[], int size)
+{
+    std::cout << "Your guesses => " << std::flush;
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << arr[i] << '\t';
+    }
+    std::cout << '\n';
+}
+
 void play_game()
 {
-    std::cout << "game is being played\n";
-    int random = rand() % 251;
-    int guess;
+    const int SIZE = 100;
+    int guesses[SIZE];
+    // int guess;
+    int guess_count = 0;
+    int random = rand() % 11;
     std::cout << random << std::endl;
     std::cout << "Enter your guess: " << std::flush;
-    while (true)
+
+    for (int i = 0; i < SIZE; i++)
     {
-        std::cin >> guess;
-        if (guess == random)
+        if (std::cin >> guesses[i])
         {
-            std::cout << "you got it right xD.." << std::endl;
-            break;
-        }
-        else if (guess < random)
-        {
-            std::cout << "Too low" << std::endl;
+            guess_count++;
+            if (guesses[i] == random)
+            {
+                std::cout << "you win xD" << std::endl;
+                break;
+            }
+            else if (guesses[i] < random)
+            {
+                std::cout << "Too low" << std ::endl;
+            }
+            else
+            {
+                std::cout << "Too high" << std::endl;
+            }
         }
         else
         {
-            std::cout << "Too high" << std::endl;
+            break;
         }
     }
+    print_array(guesses, guess_count);
 }
+
+int show_menu()
+{
+    int choice;
+    bool exp = false;
+    std::cout << "Are your ready to play T = 1 / F = 0 ? " << std::flush;
+    std::cin >> exp;
+
+    if (exp)
+    {
+        do
+        {
+            std::cout
+                << "0. Quit \n"
+                << "1. Play" << std::endl;
+            std::cout << "Enter your Choice: " << std::flush;
+            std::cin >> choice;
+
+            switch (choice)
+            {
+            case 0:
+                std::cout << "Thanks for playing, hope to see you again <3" << std::endl;
+                return 0;
+            case 1:
+                std::cout << "yo let\'s play " << std::endl;
+                play_game();
+                break;
+            }
+        } while (choice != 0);
+    }
+    else
+    {
+        std::cout << "Good Bye.." << std::endl;
+        return 0;
+    }
+}
+
 int main()
 {
-    //to prevent repition of sequence.
     srand(time(NULL));
-    int choice;
-    do
-    {
-        std::cout << "1. Play \n"
-                  << "2. Quit\n";
-        std::cout << "Enter your Choice: " << std::flush;
-        std::cin >> choice;
-        switch (choice)
-        {
-        case 1:
-            std::cout << "yo let\'s play" << std::endl;
-            play_game();
-            break;
-        case 2:
-            std::cout << "Thanks, Hope you to play again" << std::endl;
-            return 0;
-        default:
-            std::cout << "Invalid Option" << std::endl;
-        }
-    } while (choice != 2);
+    std::string name;
+    std::cout << "Enter your name please: " << std::flush;
+    std::getline(std::cin, name);
 
-    EXIT_SUCCESS;
+    std::cout << "Welcome, " << name << "." << std::endl;
+    //show_menu();
+    if(show_menu() == 0){
+        return 0;
+    }
+    play_game();
+
+    return 0;
 }
+
+
